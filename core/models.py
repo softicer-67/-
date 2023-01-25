26fd83f0f06a6424e2c90cf4567bb6a3559b6630
Yuri <57116431+softicer-67@users.noreply.github.com>
2023-01-26 00:10:59 +0500

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,15 +1,9 @@
 from django.contrib.auth.models import AbstractUser
-from django.db import models
-# from django.contrib.auth.hashers import make_password
 
 
 class User(AbstractUser):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    username = models.CharField(max_length=100, unique=True)
-    email = models.EmailField(max_length=100, unique=True, blank=True)
+    class Meta:
+        ordering = ('username',)
 
-#     def save(self, *args, **kwargs):
-#         self.password = make_password(self.password)
-#         super(User, self).save(*args, **kwargs)
-
+    def __str__(self):
+        return self.username
