bc18c3627a4c9006c8c120ae328b5d0de266a374
Yuri <57116431+softicer-67@users.noreply.github.com>
2023-01-25 23:21:29 +0500

    Update admin.py

--- core/admin.py
+++ core/admin.py
@@ -7,4 +7,4 @@ from core.models import User
 class UserAdmin(admin.ModelAdmin):
     list_display = ['username', 'email', 'first_name', 'last_name']
     search_fields = ['username', 'email', 'first_name', 'last_name']
-    exclude = ['password']
+    # exclude = ['password']
